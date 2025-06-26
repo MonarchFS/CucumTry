b544bc4c675728f206b1070e75b1c82eae3effb1
MonarchFS <ronthegamer100@gmail.com>
2025-06-26 08:46:37 +0530

    Added TagsDemo feature file and how Cucumber Tags are used.

--- CucumTry/src/test/java/testrunner/TestRun.java
+++ CucumTry/src/test/java/testrunner/TestRun.java
@@ -6,15 +6,17 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)  //to give must
-@CucumberOptions(features = {"src/test/resources/Features/DataTable.feature"}, glue = "StepDefinitions"
-,monochrome=true,
+@CucumberOptions(features = {"src/test/resources/Features/TagsDemo.feature"}, glue = "StepDefinitions"
+,monochrome=false,
 plugin= {"html:Reports\\CucumberhtmlReports.html",
 		"json:Reports\\CucumberjsonReports.json",
 		"junit:Reports\\CucumberxmlReports.xml",
 		"pretty"
 },
-dryRun =false//set to true for dryrun to check if all stepsdefination present or not
-
+dryRun =false,//set to true for dryrun to check if all stepsdefination present or not
+tags= "@SmokeTest or @SanityTest" //all 3 scenario will execute
+//tags= "@SmokeTest and @SanityTest" //will look for scenario where both test tags are present
+//tags="@FunctionalTest" //will run all tests as its feature level tag
 
 		)//path where feature file and steps file is present
 
